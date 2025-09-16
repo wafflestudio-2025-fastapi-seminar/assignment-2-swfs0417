6b09e3327a72a2b30ea4297c94f6d40d248448a9
swfs0417 <apdl0012@gmail.com>
2025-09-16 15:21:36 +0900

    add router and ip

--- src/users/router.py
+++ src/users/router.py
@@ -15,7 +15,11 @@ user_router = APIRouter(prefix="/users", tags=["users"])
 
 @user_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(request: CreateUserRequest) -> UserResponse:
-    pass
+    global user_db
+    uid = len(user_db) + 1
+    data = request.model_dump()
+    
+    user_db.append({"user_id": uid} + data)
 
 @user_router.get("/me")
 def get_user_info():
