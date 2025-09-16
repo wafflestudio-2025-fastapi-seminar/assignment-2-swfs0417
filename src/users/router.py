a2e4b449f389c70009d78e14473dd9b15b0e4cc3
swfs0417 <apdl0012@gmail.com>
2025-09-16 22:13:30 +0900

    POST /api/users/ 수정, import 경로 수정

--- src/users/router.py
+++ src/users/router.py
@@ -9,7 +9,7 @@ from fastapi import (
 )
 
 from src.users.schemas import CreateUserRequest, UserResponse
-from common.database import blocked_token_db, session_db, user_db
+from src.common.database import blocked_token_db, session_db, user_db
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
@@ -19,7 +19,11 @@ def create_user(request: CreateUserRequest) -> UserResponse:
     uid = len(user_db) + 1
     data = request.model_dump()
     
-    user_db.append({"user_id": uid} + data)
+    new_data = {"user_id": uid} + data
+    
+    user_db.append(new_data)
+    
+    return UserResponse(**new_data)
 
 @user_router.get("/me")
 def get_user_info():
