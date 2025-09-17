8632ee8a009e690982a66279bae2da968028224a
swfs0417 <apdl0012@gmail.com>
2025-09-17 23:40:27 +0900

    add hash passsword, add token_login(not done)

--- src/auth/router.py
+++ src/auth/router.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter
 from fastapi import Depends, Cookie
 
 from src.common.database import blocked_token_db, session_db, user_db
+from src.auth.schemas import AuthenticationRequest
+
+from argon2 import PasswordHasher, exceptions
+
+ph = PasswordHasher()
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -9,15 +14,24 @@ SHORT_SESSION_LIFESPAN = 15
 LONG_SESSION_LIFESPAN = 24 * 60
 
 @auth_router.post("/token")
+def token_login(request: AuthenticationRequest):
+  for user in user_db:
+    if user['email'] == request.email:
+      print(ph.hash(request.password))
+      ph.verify(user['hashed_password'], request.password)
+      
+      return #token
+  else:
+    raise exceptions.VerifyMismatchError
 
 
-@auth_router.post("/token/refresh")
+# @auth_router.post("/token/refresh")
 
 
-@auth_router.delete("/token")
+# @auth_router.delete("/token")
 
 
-@auth_router.post("/session")
+# @auth_router.post("/session")
 
 
-@auth_router.delete("/session")
+# @auth_router.delete("/session")
--- src/users/router.py
+++ src/users/router.py
@@ -1,30 +1,31 @@
 from typing import Annotated
 
-from fastapi import (
-    APIRouter,
-    Depends,
-    Cookie,
-    Header,
-    status
-)
+from fastapi import APIRouter, Depends, Cookie, Header, status
 
 from src.users.schemas import CreateUserRequest, UserResponse
 from src.common.database import blocked_token_db, session_db, user_db
+from argon2 import PasswordHasher
 
 user_router = APIRouter(prefix="/users", tags=["users"])
+ph = PasswordHasher()
+
 
 @user_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(request: CreateUserRequest) -> UserResponse:
-    global user_db
-    uid = len(user_db) + 1
-    data = request.model_dump()
-    
-    new_data = {"user_id": uid} | data
-    
-    user_db.append(new_data)
-    
-    return UserResponse(**new_data)
+  global user_db
+  uid = len(user_db) + 1
+  data = request.model_dump()
+
+  new_data = {"user_id": uid} | data
+
+  new_data["hashed_password"] = ph.hash(data["password"])
+  del new_data["password"]
+  print(new_data)
+  user_db.append(new_data)
+
+  return UserResponse(**new_data)
+
 
 @user_router.get("/me")
 def get_user_info():
-    pass
+  pass
